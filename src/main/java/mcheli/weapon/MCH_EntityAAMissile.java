b83c1186250a1e5c3821a3fae605bde7af2076d5
RagexPrince683 <yellowtoad91@gmail.com>
2024-04-18 23:59:56 -0400

    reset
    yeah idk

--- src/main/java/mcheli/weapon/MCH_EntityAAMissile.java
+++ src/main/java/mcheli/weapon/MCH_EntityAAMissile.java
@@ -7,9 +7,6 @@ import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
 public class MCH_EntityAAMissile extends MCH_EntityBaseBullet {
-   double speed = Math.sqrt(super.motionX * super.motionX + super.motionY * super.motionY + super.motionZ * super.motionZ);
-   private double maxTurningAngle = 180.0 + speed * 0.1; // Maximum turning angle in degrees
-
 
    public MCH_EntityAAMissile(World par1World) {
       super(par1World);
@@ -17,13 +14,7 @@ public class MCH_EntityAAMissile extends MCH_EntityBaseBullet {
    }
 
    public MCH_EntityAAMissile(World par1World, double posX, double posY, double posZ, double targetX, double targetY, double targetZ, float yaw, float pitch, double acceleration) {
-
       super(par1World, posX, posY, posZ, targetX, targetY, targetZ, yaw, pitch, acceleration);
-      super.targetEntity = null;
-   }
-
-   public void setMaxTurningAngle(float angle) {
-      maxTurningAngle = angle;
    }
 
    public void onUpdate() {
@@ -32,17 +23,16 @@ public class MCH_EntityAAMissile extends MCH_EntityBaseBullet {
          this.spawnParticle(this.getInfo().trajectoryParticleName, 3, 7.0F * this.getInfo().smokeSize * 0.5F);
       }
 
-      if (!super.worldObj.isRemote && this.getInfo() != null) {
-         if (super.shootingEntity != null && super.targetEntity != null && !super.targetEntity.isDead) {
+      if(!super.worldObj.isRemote && this.getInfo() != null) {
+         if(super.shootingEntity != null && super.targetEntity != null && !super.targetEntity.isDead) {
             double x = super.posX - super.targetEntity.posX;
             double y = super.posY - super.targetEntity.posY;
             double z = super.posZ - super.targetEntity.posZ;
-            //square of distance between coordinates
             double d = x * x + y * y + z * z;
             if(d > 3422500.0D) {
                this.setDead();
             } else if(this.getCountOnUpdate() > this.getInfo().rigidityTime) {
-               if (this.getInfo().proximityFuseDist >= 0.1F && d < (double) this.getInfo().proximityFuseDist) {
+               if(this.getInfo().proximityFuseDist >= 0.1F && d < (double)this.getInfo().proximityFuseDist) {
                   MovingObjectPosition mop = new MovingObjectPosition(super.targetEntity);
                   super.posX = (super.targetEntity.posX + super.posX) / 2.0D;
                   super.posY = (super.targetEntity.posY + super.posY) / 2.0D;
@@ -52,75 +42,11 @@ public class MCH_EntityAAMissile extends MCH_EntityBaseBullet {
                   this.guidanceToTarget(super.targetEntity.posX, super.targetEntity.posY, super.targetEntity.posZ);
                }
             }
-            // Rest of the original onUpdate logic
-
-            // Replace this line
-            // this.guidanceToTarget(super.targetEntity.posX, super.targetEntity.posY, super.targetEntity.posZ);
-
-            // With this line
-            double missileHeading = Math.toDegrees(Math.atan2(super.motionZ, super.motionX));
-            missileHeading = (missileHeading + 360) % 360;
-            System.out.println(missileHeading);
-            double angleToTarget = Math.abs(missileHeading - rotationYaw);
-            if (angleToTarget > maxTurningAngle) {
-               this.newExplosion(super.posX, super.posY, super.posZ, 1, 1, false);
-               this.setDead();
-            } else {
-               this.guidanceToTargetLimited(super.targetEntity.posX, super.targetEntity.posY, super.targetEntity.posZ);
-            }
-
-            // Rest of the original onUpdate logic
          } else {
-            this.newExplosion(super.posX, super.posY, super.posZ, 1, 1, false);
             this.setDead();
          }
       }
-   }
 
-   private void guidanceToTargetLimited(double targetX, double targetY, double targetZ) {
-      if (super.getInfo() != null) {
-         double deltaX = targetX - super.posX;
-         double deltaY = targetY - super.posY;
-         double deltaZ = targetZ - super.posZ;
-
-         double desiredHeading = Math.toDegrees(Math.atan2(deltaZ, deltaX));
-         double currentHeading = Math.toDegrees(Math.atan2(super.motionZ, super.motionX));
-         double headingError = desiredHeading - currentHeading;
-         headingError = (headingError + 180) % 360 - 180;
-         double adjustmentFactor = 0.1; // Adjust this value as needed
-         double adjustedHeading = currentHeading + headingError * adjustmentFactor;
-         double adjustedHeadingRad = Math.toRadians(adjustedHeading);
-         double adjustedMotionX = Math.cos(adjustedHeadingRad);
-         double adjustedMotionZ = Math.sin(adjustedHeadingRad);
-         super.motionX = adjustedMotionX;
-         super.motionZ = adjustedMotionZ;
-         super.rotationYaw = (float) adjustedHeading;
-
-
-
-
-         double distanceToTarget = Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
-
-         if (distanceToTarget > 0) {
-            double maxDelta = maxTurningAngle * Math.PI / 180.0;
-            double angleScale = Math.min(1.0, maxDelta / distanceToTarget);
-
-            double adjustedDeltaX = deltaX * angleScale;
-            double adjustedDeltaY = deltaY * angleScale;
-            double adjustedDeltaZ = deltaZ * angleScale;
-
-            double adjustedTargetX = super.posX + adjustedDeltaX;
-            double adjustedTargetY = super.posY + adjustedDeltaY;
-            double adjustedTargetZ = super.posZ + adjustedDeltaZ;
-
-            super.rotationYaw = (float) Math.toDegrees(Math.atan2(adjustedDeltaZ, adjustedDeltaX));
-            super.rotationPitch = (float) Math.toDegrees(Math.atan2(-adjustedDeltaY, Math.sqrt(adjustedDeltaX * adjustedDeltaX + adjustedDeltaZ * adjustedDeltaZ)));
-
-            super.motionX = adjustedDeltaX;
-            super.motionY = adjustedDeltaY;
-            super.motionZ = adjustedDeltaZ;
-         }
-      }
    }
 
    public MCH_BulletModel getDefaultBulletModel() {
