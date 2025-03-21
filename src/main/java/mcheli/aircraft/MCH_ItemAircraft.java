c1ad0371cadb5b4d748c4f57ead42497022d14ed
RagexPrince683 <yellowtoad91@gmail.com>
2025-03-20 20:33:19 -0400

    oh wow that *really* worked
    it didn't

--- src/main/java/mcheli/aircraft/MCH_ItemAircraft.java
+++ src/main/java/mcheli/aircraft/MCH_ItemAircraft.java
@@ -1,12 +1,10 @@
 package mcheli.aircraft;
 
-import java.util.List;
-import mcheli.MCH_Achievement;
+import java.util.*;
+
 import mcheli.MCH_Config;
 import mcheli.MCH_MOD;
-import mcheli.aircraft.MCH_AircraftInfo;
-import mcheli.aircraft.MCH_EntityAircraft;
-import mcheli.aircraft.MCH_ItemAircraftDispenseBehavior;
+
 import mcheli.wrapper.W_EntityPlayer;
 import mcheli.wrapper.W_Item;
 import mcheli.wrapper.W_MovingObjectPosition;
@@ -25,6 +23,11 @@ import net.minecraft.world.World;
 public abstract class MCH_ItemAircraft extends W_Item {
 
    private static boolean isRegistedDispenseBehavior = false;
+   private static final int PLACEMENT_DELAY = 5; // Delay in seconds
+   private static final Map<String, Long> playerCooldowns = new HashMap<>();
+
+   private static final int SPAWN_DELAY_TICKS = 100; // 5 seconds (100 ticks)
+   private final Map<UUID, Long> spawnDelays = new HashMap<>();
 
 
    public MCH_ItemAircraft(int i) {
@@ -43,14 +46,15 @@ public abstract class MCH_ItemAircraft extends W_Item {
       MCH_EntityAircraft ac = createAircraft(player.worldObj, -1.0D, -1.0D, -1.0D, stack);
       if (ac != null &&
               ac.isNewUAV()) {
-         lines.add(EnumChatFormatting.RED + "DANGER!");
-         lines.add(EnumChatFormatting.RED + "This drone has a new UAV mechanic!");
-         lines.add(EnumChatFormatting.RED + "It may contain a lot of bugs!");
+         lines.add(EnumChatFormatting.RED + "WARNING!");
+         lines.add(EnumChatFormatting.RED + "This drone has the new UAV mechanic!");
+         lines.add(EnumChatFormatting.RED + "It may contain bugs!");
          lines.add(EnumChatFormatting.RED + "Clear your inventory before use!");
       }
 //
       super.addInformation(stack, player, lines, par4);
    }
+   //todo add a variable that allows you to add desc for other items too not just new uavs
 
    public abstract MCH_AircraftInfo getAircraftInfo();
 
@@ -71,6 +75,7 @@ public abstract class MCH_ItemAircraft extends W_Item {
          //   ac.onAcInfoReloaded();
          //}
          //causes a crash when the fucking model is not loaded
+         //fixed models not rendering as of MCHO hotfix
          ac.initRotationYaw((float)(((MathHelper.floor_double((double)(rotationYaw * 4.0F / 360.0F) + 0.5D) & 3) - 1) * 90));
          return !world.getCollidingBoundingBoxes(ac, ac.boundingBox.expand(-0.1D, -0.1D, -0.1D)).isEmpty()?null:ac;
       }
@@ -81,13 +86,26 @@ public abstract class MCH_ItemAircraft extends W_Item {
       return info != null?super.toString() + "(" + info.getDirectoryName() + ":" + info.name + ")":super.toString() + "(null)";
    }
 
+   @Override
    public ItemStack onItemRightClick(ItemStack par1ItemStack, World world, EntityPlayer player) {
+      UUID playerId = player.getUniqueID();
+      long currentTime = world.getTotalWorldTime();
+
+      // Check if the player has a spawn delay
+      if (spawnDelays.containsKey(playerId)) {
+         long timeLeft = (spawnDelays.get(playerId) + SPAWN_DELAY_TICKS) - currentTime;
+         if (timeLeft > 0) {
+            player.addChatMessage(new ChatComponentText("You must wait " + (timeLeft / 20) + " seconds before spawning another aircraft."));
+            return par1ItemStack;
+         }
+      }
+
       float f = 1.0F;
       float f1 = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * f;
       float f2 = player.prevRotationYaw + (player.rotationYaw - player.prevRotationYaw) * f;
-      double d0 = player.prevPosX + (player.posX - player.prevPosX) * (double)f;
-      double d1 = player.prevPosY + (player.posY - player.prevPosY) * (double)f + 1.62D - (double)player.yOffset;
-      double d2 = player.prevPosZ + (player.posZ - player.prevPosZ) * (double)f;
+      double d0 = player.prevPosX + (player.posX - player.prevPosX) * (double) f;
+      double d1 = player.prevPosY + (player.posY - player.prevPosY) * (double) f + 1.62D - (double) player.yOffset;
+      double d2 = player.prevPosZ + (player.posZ - player.prevPosZ) * (double) f;
       Vec3 vec3 = W_WorldFunc.getWorldVec3(world, d0, d1, d2);
       float f3 = MathHelper.cos(-f2 * 0.017453292F - 3.1415927F);
       float f4 = MathHelper.sin(-f2 * 0.017453292F - 3.1415927F);
@@ -96,50 +114,71 @@ public abstract class MCH_ItemAircraft extends W_Item {
       float f7 = f4 * f5;
       float f8 = f3 * f5;
       double d3 = 5.0D;
-      Vec3 vec31 = vec3.addVector((double)f7 * d3, (double)f6 * d3, (double)f8 * d3);
+      Vec3 vec31 = vec3.addVector((double) f7 * d3, (double) f6 * d3, (double) f8 * d3);
       MovingObjectPosition mop = W_WorldFunc.clip(world, vec3, vec31, true);
-      if(mop == null) {
+
+      if (mop == null) {
          return par1ItemStack;
-      } else {
-         Vec3 vec32 = player.getLook(f);
-         boolean flag = false;
-         float f9 = 1.0F;
-         List list = world.getEntitiesWithinAABBExcludingEntity(player, player.boundingBox.addCoord(vec32.xCoord * d3, vec32.yCoord * d3, vec32.zCoord * d3).expand((double)f9, (double)f9, (double)f9));
+      }
 
-         for(int i = 0; i < list.size(); ++i) {
-            Entity block = (Entity)list.get(i);
-            if(block.canBeCollidedWith()) {
-               float f10 = block.getCollisionBorderSize();
-               AxisAlignedBB axisalignedbb = block.boundingBox.expand((double)f10, (double)f10, (double)f10);
-               if(axisalignedbb.isVecInside(vec3)) {
-                  flag = true;
-               }
+      Vec3 vec32 = player.getLook(f);
+      boolean flag = false;
+      float f9 = 1.0F;
+      List<Entity> list = world.getEntitiesWithinAABBExcludingEntity(player, player.boundingBox.addCoord(vec32.xCoord * d3, vec32.yCoord * d3, vec32.zCoord * d3).expand((double) f9, (double) f9, (double) f9));
+
+      for (Entity block : list) {
+         if (block.canBeCollidedWith()) {
+            float f10 = block.getCollisionBorderSize();
+            AxisAlignedBB axisalignedbb = block.boundingBox.expand((double) f10, (double) f10, (double) f10);
+            if (axisalignedbb.isVecInside(vec3)) {
+               flag = true;
             }
-         }
-
-         if(flag) {
-            return par1ItemStack;
-         } else {
-            if(W_MovingObjectPosition.isHitTypeTile(mop)) {
-               MCH_Config var10000 = MCH_MOD.config;
-               if(MCH_Config.PlaceableOnSpongeOnly.prmBool) {
-                  Block var32 = world.getBlock(mop.blockX, mop.blockY, mop.blockZ);
-                  if(!(var32 instanceof BlockSponge)) {
-                     return par1ItemStack;
-                  }
-               }
-
-
-               this.spawnAircraft(par1ItemStack, world, player, mop.blockX, mop.blockY, mop.blockZ);
-            }
-
-            return par1ItemStack;
          }
       }
+
+      if (flag) {
+         return par1ItemStack;
+      }
+
+      if (W_MovingObjectPosition.isHitTypeTile(mop)) {
+         MCH_Config var10000 = MCH_MOD.config;
+         if (MCH_Config.PlaceableOnSpongeOnly.prmBool) {
+            Block var32 = world.getBlock(mop.blockX, mop.blockY, mop.blockZ);
+            if (!(var32 instanceof BlockSponge)) {
+               return par1ItemStack;
+            }
+         }
+
+         // Set the delay before allowing another spawn
+         spawnDelays.put(playerId, currentTime);
+
+         this.spawnAircraft(par1ItemStack, world, player, mop.blockX, mop.blockY, mop.blockZ);
+      }
+
+      return par1ItemStack;
    }
 
    public MCH_EntityAircraft spawnAircraft(ItemStack itemStack, World world, EntityPlayer player, int x, int y, int z) {
+      String playerName = player.getCommandSenderName();
+      long currentTime = System.currentTimeMillis();
 
+      if (playerCooldowns.containsKey(playerName)) {
+         long lastPlacementTime = playerCooldowns.get(playerName);
+         if ((currentTime - lastPlacementTime) < (PLACEMENT_DELAY * 1000)) {
+            if (world.isRemote) {
+               W_EntityPlayer.addChatMessage(player, "You must wait before placing another aircraft.");
+            }
+            return null;
+         }
+      }
+
+      playerCooldowns.put(playerName, currentTime);
+
+      try {
+         Thread.sleep(PLACEMENT_DELAY * 1000); // Delay before spawning aircraft
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
 
       MCH_EntityAircraft ac = this.onTileClick(itemStack, world, player.rotationYaw, x, y, z);
       if(ac != null) {
@@ -148,26 +187,13 @@ public abstract class MCH_ItemAircraft extends W_Item {
                if(ac.isSmallUAV()) {
                   W_EntityPlayer.addChatMessage(player, "Please use the UAV station OR Portable Controller");
                } else {
-                  W_EntityPlayer.addChatMessage(player, "Please use the UAV station");
+                  W_EntityPlayer.addChatMessage(player, "Please use the UAV station to control this vehicle.");
                }
             }
+         }
 
-            ac = null;
-         } else {
-            if(!world.isRemote) {
-               ac.getAcDataFromItem(itemStack);
-               world.spawnEntityInWorld(ac);
-               //crashes randomly?
-               //if(ac.getAcInfo() != null) {
-               //   ac.getAcInfo().reload();
-               //   //ac.onAcInfoReloaded();
-               //}
-               MCH_Achievement.addStat(player, MCH_Achievement.welcome, 1);
-            }
-
-            if(!player.capabilities.isCreativeMode) {
-               --itemStack.stackSize;
-            }
+         if (!world.isRemote) {
+            world.spawnEntityInWorld(ac);
          }
       }
 
