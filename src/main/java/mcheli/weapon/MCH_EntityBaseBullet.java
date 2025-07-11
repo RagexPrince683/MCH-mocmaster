b931b5154330880a84b6f55d48cd5a4dc7ccf635
RagexPrince683 <yellowtoad91@gmail.com>
2025-07-11 05:15:33 -0400

    Update MCH_EntityBaseBullet.java

--- src/main/java/mcheli/weapon/MCH_EntityBaseBullet.java
+++ src/main/java/mcheli/weapon/MCH_EntityBaseBullet.java
@@ -162,17 +162,29 @@ public abstract class MCH_EntityBaseBullet extends W_Entity implements MCH_IChun
 
     // Chunk loading code courtesy of HBM's nuclear tech mod https://github.com/HbmMods/Hbm-s-Nuclear-Tech-GIT/
 
+    //public void init(Ticket ticket) {
+    //    if (!worldObj.isRemote) {
+    //        if (ticket != null) {
+    //            if (loaderTicket == null) {
+    //                loaderTicket = ticket;
+    //                loaderTicket.bindEntity(this);
+    //                loaderTicket.getModData();
+    //            }
+    //            // Force load the initial chunk where the bullet is spawned
+    //            ForgeChunkManager.forceChunk(loaderTicket, new ChunkCoordIntPair(chunkCoordX, chunkCoordZ));
+    //        }
+    //    }
+    //}
+
     public void init(Ticket ticket) {
-        if (!worldObj.isRemote) {
-            if (ticket != null) {
-                if (loaderTicket == null) {
-                    loaderTicket = ticket;
-                    loaderTicket.bindEntity(this);
-                    loaderTicket.getModData();
-                }
-                // Force load the initial chunk where the bullet is spawned
-                ForgeChunkManager.forceChunk(loaderTicket, new ChunkCoordIntPair(chunkCoordX, chunkCoordZ));
+        if (!worldObj.isRemote && ticket != null && shouldLoadChunks()) {
+            if (loaderTicket == null) {
+                loaderTicket = ticket;
+                loaderTicket.bindEntity(this);
+                loaderTicket.getModData(); // optional metadata
             }
+
+            ForgeChunkManager.forceChunk(loaderTicket, new ChunkCoordIntPair(chunkCoordX, chunkCoordZ));
         }
     }
 
