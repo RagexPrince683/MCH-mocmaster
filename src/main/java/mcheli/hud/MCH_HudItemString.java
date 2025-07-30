3acd824d6619e98ab245fd67abeb796b30bbb2d4
RagexPrince683 <yellowtoad91@gmail.com>
2025-07-30 14:14:26 -0400

    Revert ">try doing something simple"
    This reverts commit 97b8454616bb3130a9704fdc89684686afe7e9ce.

--- src/main/java/mcheli/hud/MCH_HudItemString.java
+++ src/main/java/mcheli/hud/MCH_HudItemString.java
@@ -135,11 +135,9 @@ public class MCH_HudItemString extends MCH_HudItem {
             if (MCH_HudItem.CurrentWeapon == null) {
                return;
             }
-            //prm[i] = (float)MCH_HudItem.MortarDist >= 0.0F
-            //        ? MCH_HudItem.mortarDistStr
-            //        : "";
-            //possible culprit
-            prm[i] = Float.valueOf(MCH_HudItem.MortarDist);
+            prm[i] = (float)MCH_HudItem.MortarDist >= 0.0F
+                    ? MCH_HudItem.mortarDistStr
+                    : "";
             break;
          case 23:
             prm[i] = "1.7.10";
