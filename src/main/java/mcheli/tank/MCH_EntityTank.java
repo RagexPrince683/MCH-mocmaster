72a2270238fc85d7eebccee037ee55388a6b265a
RagexPrince683 <yellowtoad91@gmail.com>
2026-01-20 18:51:48 -0500

    Revert "does nothing award"
    This reverts commit 26235b3ac593169a8b484f222b1a75fc8a1b74e3.

--- src/main/java/mcheli/tank/MCH_EntityTank.java
+++ src/main/java/mcheli/tank/MCH_EntityTank.java
@@ -104,16 +104,7 @@ public class MCH_EntityTank extends MCH_EntityAircraft {
          this.switchFreeLookModeClient(this.getAcInfo().defaultFreelook);
          super.weapons = this.createWeapon(1 + this.getSeatNum());
          this.initPartRotation(this.getRotYaw(), this.getRotPitch());
-         double comZ = this.getTankInfo().weightedCenterZ;
-
-// scale COM → wheel offset (visual only)
-         double wheelZ = -comZ * 0.25D;
-
-         this.WheelMng.createWheels(
-                 super.worldObj,
-                 this.getAcInfo().wheels,
-                 Vec3.createVectorHelper(0.0D, -0.35D, wheelZ)
-         );
+         this.WheelMng.createWheels(super.worldObj, this.getAcInfo().wheels, Vec3.createVectorHelper(0.0D, -0.35D, (double)this.getTankInfo().weightedCenterZ));
       }
 
    }
