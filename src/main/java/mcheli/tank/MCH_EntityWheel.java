e2cb114e3b66be55752ea237aa50fdd8eeb22c28
RagexPrince683 <yellowtoad91@gmail.com>
2026-01-20 19:42:01 -0500

    1.12.2 tier shitlow logic

--- src/main/java/mcheli/tank/MCH_EntityWheel.java
+++ src/main/java/mcheli/tank/MCH_EntityWheel.java
@@ -26,7 +26,6 @@ public class MCH_EntityWheel extends W_Entity {
    public Vec3 pos;
    boolean isPlus;
 
-
    public MCH_EntityWheel(World w) {
       super(w);
       this.setSize(1.0F, 1.0F);
@@ -39,10 +38,6 @@ public class MCH_EntityWheel extends W_Entity {
       this.pos = pos;
       this.isPlus = pos.zCoord >= weightedCenter.zCoord;
    }
-
-   //public MCH_BoundingBox copy() {
-   //   return new MCH_BoundingBox(this.offsetX, this.offsetY, this.offsetZ, this.width, this.height, this.damegeFactor);
-   //}
 
    public void travelToDimension(int p_71027_1_) {}
 
@@ -62,6 +57,13 @@ public class MCH_EntityWheel extends W_Entity {
 
    public void moveEntity(double parX, double parY, double parZ) {
       super.worldObj.theProfiler.startSection("move");
+
+      // Clamp very large vertical requests to avoid teleportation that leads to collision feedback.
+      // This prevents wheels from being moved hundreds of blocks in a single tick by collision correction.
+      double maxVerticalMovePerTick = 1.5D; // tune down if needed
+      if (parY > maxVerticalMovePerTick) parY = maxVerticalMovePerTick;
+      if (parY < -maxVerticalMovePerTick) parY = -maxVerticalMovePerTick;
+
       super.ySize *= 0.4F;
       double nowPosX = super.posX;
       double nowPosY = super.posY;
@@ -72,62 +74,62 @@ public class MCH_EntityWheel extends W_Entity {
       AxisAlignedBB axisalignedbb = super.boundingBox.copy();
       List list = this.getCollidingBoundingBoxes(this, super.boundingBox.addCoord(parX, parY, parZ));
 
-      for(int flag1 = 0; flag1 < list.size(); ++flag1) {
-         parY = ((AxisAlignedBB)list.get(flag1)).calculateYOffset(super.boundingBox, parY);
+      for (int flag1 = 0; flag1 < list.size(); ++flag1) {
+         parY = ((AxisAlignedBB) list.get(flag1)).calculateYOffset(super.boundingBox, parY);
       }
 
       super.boundingBox.offset(0.0D, parY, 0.0D);
       boolean var32 = super.onGround || my != parY && my < 0.0D;
 
       int bkParY;
-      for(bkParY = 0; bkParY < list.size(); ++bkParY) {
-         parX = ((AxisAlignedBB)list.get(bkParY)).calculateXOffset(super.boundingBox, parX);
+      for (bkParY = 0; bkParY < list.size(); ++bkParY) {
+         parX = ((AxisAlignedBB) list.get(bkParY)).calculateXOffset(super.boundingBox, parX);
       }
 
       super.boundingBox.offset(parX, 0.0D, 0.0D);
 
-      for(bkParY = 0; bkParY < list.size(); ++bkParY) {
-         parZ = ((AxisAlignedBB)list.get(bkParY)).calculateZOffset(super.boundingBox, parZ);
+      for (bkParY = 0; bkParY < list.size(); ++bkParY) {
+         parZ = ((AxisAlignedBB) list.get(bkParY)).calculateZOffset(super.boundingBox, parZ);
       }
 
       super.boundingBox.offset(0.0D, 0.0D, parZ);
-      if(super.stepHeight > 0.0F && var32 && super.ySize < 0.05F && (mx != parX || mz != parZ)) {
+      if (super.stepHeight > 0.0F && var32 && super.ySize < 0.05F && (mx != parX || mz != parZ)) {
          double bkParX = parX;
          double var33 = parY;
          double bkParZ = parZ;
          parX = mx;
-         parY = (double)super.stepHeight;
+         parY = (double) super.stepHeight;
          parZ = mz;
          AxisAlignedBB throwable = super.boundingBox.copy();
          super.boundingBox.setBB(axisalignedbb);
          list = this.getCollidingBoundingBoxes(this, super.boundingBox.addCoord(mx, parY, mz));
 
          int crashreport;
-         for(crashreport = 0; crashreport < list.size(); ++crashreport) {
-            parY = ((AxisAlignedBB)list.get(crashreport)).calculateYOffset(super.boundingBox, parY);
+         for (crashreport = 0; crashreport < list.size(); ++crashreport) {
+            parY = ((AxisAlignedBB) list.get(crashreport)).calculateYOffset(super.boundingBox, parY);
          }
 
          super.boundingBox.offset(0.0D, parY, 0.0D);
 
-         for(crashreport = 0; crashreport < list.size(); ++crashreport) {
-            parX = ((AxisAlignedBB)list.get(crashreport)).calculateXOffset(super.boundingBox, parX);
+         for (crashreport = 0; crashreport < list.size(); ++crashreport) {
+            parX = ((AxisAlignedBB) list.get(crashreport)).calculateXOffset(super.boundingBox, parX);
          }
 
          super.boundingBox.offset(parX, 0.0D, 0.0D);
 
-         for(crashreport = 0; crashreport < list.size(); ++crashreport) {
-            parZ = ((AxisAlignedBB)list.get(crashreport)).calculateZOffset(super.boundingBox, parZ);
+         for (crashreport = 0; crashreport < list.size(); ++crashreport) {
+            parZ = ((AxisAlignedBB) list.get(crashreport)).calculateZOffset(super.boundingBox, parZ);
          }
 
          super.boundingBox.offset(0.0D, 0.0D, parZ);
-         parY = (double)(-super.stepHeight);
+         parY = (double) (-super.stepHeight);
 
-         for(crashreport = 0; crashreport < list.size(); ++crashreport) {
-            parY = ((AxisAlignedBB)list.get(crashreport)).calculateYOffset(super.boundingBox, parY);
+         for (crashreport = 0; crashreport < list.size(); ++crashreport) {
+            parY = ((AxisAlignedBB) list.get(crashreport)).calculateYOffset(super.boundingBox, parY);
          }
 
          super.boundingBox.offset(0.0D, parY, 0.0D);
-         if(bkParX * bkParX + bkParZ * bkParZ >= parX * parX + parZ * parZ) {
+         if (bkParX * bkParX + bkParZ * bkParZ >= parX * parX + parZ * parZ) {
             parX = bkParX;
             parY = var33;
             parZ = bkParZ;
@@ -138,22 +140,22 @@ public class MCH_EntityWheel extends W_Entity {
       super.worldObj.theProfiler.endSection();
       super.worldObj.theProfiler.startSection("rest");
       super.posX = (super.boundingBox.minX + super.boundingBox.maxX) / 2.0D;
-      super.posY = super.boundingBox.minY + (double)super.yOffset - (double)super.ySize;
+      super.posY = super.boundingBox.minY + (double) super.yOffset - (double) super.ySize;
       super.posZ = (super.boundingBox.minZ + super.boundingBox.maxZ) / 2.0D;
       super.isCollidedHorizontally = mx != parX || mz != parZ;
       super.isCollidedVertically = my != parY;
       super.onGround = my != parY && my < 0.0D;
       super.isCollided = super.isCollidedHorizontally || super.isCollidedVertically;
       this.updateFallState(parY, super.onGround);
-      if(mx != parX) {
+      if (mx != parX) {
          super.motionX = 0.0D;
       }
 
-      if(my != parY) {
+      if (my != parY) {
          super.motionY = 0.0D;
       }
 
-      if(mz != parZ) {
+      if (mz != parZ) {
          super.motionZ = 0.0D;
       }
 
@@ -178,12 +180,12 @@ public class MCH_EntityWheel extends W_Entity {
       int i1 = MathHelper.floor_double(par2AxisAlignedBB.minZ);
       int j1 = MathHelper.floor_double(par2AxisAlignedBB.maxZ + 1.0D);
 
-      for(int d0 = i; d0 < j; ++d0) {
-         for(int l1 = i1; l1 < j1; ++l1) {
-            if(par1Entity.worldObj.blockExists(d0, 64, l1)) {
-               for(int list = k - 1; list < l; ++list) {
+      for (int d0 = i; d0 < j; ++d0) {
+         for (int l1 = i1; l1 < j1; ++l1) {
+            if (par1Entity.worldObj.blockExists(d0, 64, l1)) {
+               for (int list = k - 1; list < l; ++list) {
                   Block j2 = W_WorldFunc.getBlock(par1Entity.worldObj, d0, list, l1);
-                  if(j2 != null) {
+                  if (j2 != null) {
                      j2.addCollisionBoxesToList(par1Entity.worldObj, d0, list, l1, par2AxisAlignedBB, collidingBoundingBoxes, par1Entity);
                   }
                }
@@ -194,16 +196,16 @@ public class MCH_EntityWheel extends W_Entity {
       double var16 = 0.25D;
       List var17 = par1Entity.worldObj.getEntitiesWithinAABBExcludingEntity(par1Entity, par2AxisAlignedBB.expand(var16, var16, var16));
 
-      for(int var18 = 0; var18 < var17.size(); ++var18) {
-         Entity entity = (Entity)var17.get(var18);
-         if(!W_Lib.isEntityLivingBase(entity) && !(entity instanceof MCH_EntitySeat) && !(entity instanceof MCH_EntityHitBox) && entity != this.parents) {
+      for (int var18 = 0; var18 < var17.size(); ++var18) {
+         Entity entity = (Entity) var17.get(var18);
+         if (!W_Lib.isEntityLivingBase(entity) && !(entity instanceof MCH_EntitySeat) && !(entity instanceof MCH_EntityHitBox) && entity != this.parents) {
             AxisAlignedBB axisalignedbb1 = entity.getBoundingBox();
-            if(axisalignedbb1 != null && axisalignedbb1.intersectsWith(par2AxisAlignedBB)) {
+            if (axisalignedbb1 != null && axisalignedbb1.intersectsWith(par2AxisAlignedBB)) {
                collidingBoundingBoxes.add(axisalignedbb1);
             }
 
             axisalignedbb1 = par1Entity.getCollisionBox(entity);
-            if(axisalignedbb1 != null && axisalignedbb1.intersectsWith(par2AxisAlignedBB)) {
+            if (axisalignedbb1 != null && axisalignedbb1.intersectsWith(par2AxisAlignedBB)) {
                collidingBoundingBoxes.add(axisalignedbb1);
             }
          }
