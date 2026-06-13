2d6508a2226f78240ed9f772abd7ee07766aad05
RagexPrince683 <49804375+RagexPrince683@users.noreply.github.com>
2026-06-13 10:11:01 -0400

    Revert "Fix far-from-origin ship bounding box collision and hits"

--- src/main/java/mcheli/aircraft/MCH_EntityHitBox.java
+++ src/main/java/mcheli/aircraft/MCH_EntityHitBox.java
@@ -15,8 +15,6 @@ public class MCH_EntityHitBox extends W_Entity {
 
    public MCH_EntityBaseVehicle parent;
    public int debugId;
-   private MCH_BoundingBox extraBoundingBox;
-   private boolean physical;
 
 
    public MCH_EntityHitBox(World world) {
@@ -27,8 +25,6 @@ public class MCH_EntityHitBox extends W_Entity {
       super.motionY = 0.0D;
       super.motionZ = 0.0D;
       this.parent = null;
-      this.extraBoundingBox = null;
-      this.physical = false;
       super.ignoreFrustumCheck = true;
       super.isImmuneToFire = true;
    }
@@ -43,49 +39,16 @@ public class MCH_EntityHitBox extends W_Entity {
       this.setSize(w, h);
    }
 
-   public MCH_EntityHitBox(World world, MCH_EntityBaseVehicle ac, MCH_BoundingBox boundingBox, int index) {
-      this(world);
-      this.parent = ac;
-      this.extraBoundingBox = boundingBox;
-      this.physical = true;
-      this.debugId = index;
-      this.getDataWatcher().updateObject(16, Byte.valueOf((byte)1));
-      this.getDataWatcher().updateObject(17, Integer.valueOf(ac.getEntityId()));
-      this.getDataWatcher().updateObject(18, Float.valueOf(boundingBox.width));
-      this.getDataWatcher().updateObject(19, Float.valueOf(boundingBox.height));
-      this.setSize(boundingBox.width, boundingBox.height);
-      this.setPosition(boundingBox.nowPos.xCoord, boundingBox.nowPos.yCoord, boundingBox.nowPos.zCoord);
-   }
-
-   protected void entityInit() {
-      this.getDataWatcher().addObject(16, Byte.valueOf((byte)0));
-      this.getDataWatcher().addObject(17, Integer.valueOf(0));
-      this.getDataWatcher().addObject(18, Float.valueOf(1.0F));
-      this.getDataWatcher().addObject(19, Float.valueOf(1.0F));
-   }
-
-   public void updatePhysicalPosition() {
-      if(this.extraBoundingBox != null) {
-         this.setSize(this.extraBoundingBox.width, this.extraBoundingBox.height);
-         this.setPosition(this.extraBoundingBox.nowPos.xCoord, this.extraBoundingBox.nowPos.yCoord,
-                 this.extraBoundingBox.nowPos.zCoord);
-      }
-   }
-
-   public boolean isPhysical() {
-      return this.physical || this.getDataWatcher().getWatchableObjectByte(16) != 0;
-   }
-
    protected boolean canTriggerWalking() {
       return false;
    }
 
    public AxisAlignedBB getCollisionBox(Entity par1Entity) {
-      return this.isPhysical()?super.boundingBox:null;
+      return null;
    }
 
    public AxisAlignedBB getBoundingBox() {
-      return this.isPhysical()?super.boundingBox:null;
+      return null;
    }
 
    public boolean canBePushed() {
@@ -97,16 +60,6 @@ public class MCH_EntityHitBox extends W_Entity {
    }
 
    public boolean attackEntityFrom(DamageSource par1DamageSource, float par2) {
-      if(this.parent == null && super.worldObj.isRemote) {
-         Entity entity = super.worldObj.getEntityByID(this.getDataWatcher().getWatchableObjectInt(17));
-         if(entity instanceof MCH_EntityBaseVehicle) {
-            this.parent = (MCH_EntityBaseVehicle)entity;
-         }
-      }
-      if(this.parent != null && this.extraBoundingBox != null) {
-         this.parent.lastBBDamageFactor = this.extraBoundingBox.damegeFactor;
-         this.parent.lastHitBoundingBoxType = this.extraBoundingBox.boundingBoxType;
-      }
       return this.parent != null?this.parent.attackEntityFrom(par1DamageSource, par2):false;
    }
 
@@ -120,19 +73,6 @@ public class MCH_EntityHitBox extends W_Entity {
 
    public void onUpdate() {
       super.onUpdate();
-      if(this.isPhysical() && super.worldObj.isRemote) {
-         float width = this.getDataWatcher().getWatchableObjectFloat(18);
-         float height = this.getDataWatcher().getWatchableObjectFloat(19);
-         if(super.width != width || super.height != height) {
-            this.setSize(width, height);
-         }
-      }
-      if(this.isPhysical() && this.parent == null && super.worldObj.isRemote) {
-         Entity entity = super.worldObj.getEntityByID(this.getDataWatcher().getWatchableObjectInt(17));
-         if(entity instanceof MCH_EntityBaseVehicle) {
-            this.parent = (MCH_EntityBaseVehicle)entity;
-         }
-      }
    }
 
    protected void writeEntityToNBT(NBTTagCompound par1NBTTagCompound) {}
