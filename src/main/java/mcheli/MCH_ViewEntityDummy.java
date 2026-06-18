117c0fdec8fc329f5fee75b823d9ab3fa869f17d
Zealot <39095332+GlowingFederal@users.noreply.github.com>
2026-06-18 02:58:39 -0400

    nice illya

--- src/main/java/mcheli/MCH_ViewEntityDummy.java
+++ src/main/java/mcheli/MCH_ViewEntityDummy.java
@@ -1,6 +1,5 @@
 package mcheli;
 
-import mcheli.MCH_Camera;
 import mcheli.plane.MCP_PlaneChaseCamera;
 import mcheli.wrapper.W_Session;
 import net.minecraft.client.Minecraft;
@@ -12,8 +11,7 @@ import net.minecraft.world.World;
 public class MCH_ViewEntityDummy extends EntityPlayerSP {
 
    private static MCH_ViewEntityDummy instance = null;
-   private float zoom;
-
+   private float zoom = 1.0F;
 
    private MCH_ViewEntityDummy(World world) {
       super(Minecraft.getMinecraft(), world, W_Session.newSession(), 0);
@@ -25,6 +23,7 @@ public class MCH_ViewEntityDummy extends EntityPlayerSP {
    public static MCH_ViewEntityDummy getInstance(World w) {
       if((instance == null || instance.isDead) && w.isRemote) {
          instance = new MCH_ViewEntityDummy(w);
+
          if(Minecraft.getMinecraft().thePlayer != null) {
             instance.movementInput = Minecraft.getMinecraft().thePlayer.movementInput;
          }
@@ -41,22 +40,25 @@ public class MCH_ViewEntityDummy extends EntityPlayerSP {
          instance.setDead();
          instance = null;
       }
-
    }
 
-   public void onUpdate() {}
+   @Override
+   public void onUpdate() {
+   }
 
    public void update(MCH_Camera camera) {
       if(MCP_PlaneChaseCamera.shouldBlockNonChaseDummyWrite("MCH_ViewEntityDummy.update")) {
          return;
       }
+
       if(camera != null) {
          this.zoom = camera.getCameraZoom();
-         super.prevRotationYaw = super.rotationYaw;
-         super.prevRotationPitch = super.rotationPitch;
+
+         super.prevRotationYaw = camera.rotationYaw;
+         super.prevRotationPitch = camera.rotationPitch;
          super.rotationYaw = camera.rotationYaw;
          super.rotationPitch = camera.rotationPitch;
-         MCP_PlaneChaseCamera.recordDummyTransformWrite("MCH_ViewEntityDummy.update", false);
+
          super.prevPosX = camera.posX;
          super.prevPosY = camera.posY;
          super.prevPosZ = camera.posZ;
@@ -66,8 +68,14 @@ public class MCH_ViewEntityDummy extends EntityPlayerSP {
          super.posX = camera.posX;
          super.posY = camera.posY;
          super.posZ = camera.posZ;
+
          this.configureNoCollisionChaseDummy();
-         MCP_PlaneChaseCamera.logCameraWrite("MCH_ViewEntityDummy.update", String.format("pos=(%.3f,%.3f,%.3f)", Double.valueOf(super.posX), Double.valueOf(super.posY), Double.valueOf(super.posZ)));
+
+         MCP_PlaneChaseCamera.recordDummyTransformWrite("MCH_ViewEntityDummy.update", false);
+         MCP_PlaneChaseCamera.logCameraWrite(
+                 "MCH_ViewEntityDummy.update",
+                 String.format("pos=(%.3f,%.3f,%.3f)", Double.valueOf(super.posX), Double.valueOf(super.posY), Double.valueOf(super.posZ))
+         );
       }
    }
 
@@ -75,8 +83,8 @@ public class MCH_ViewEntityDummy extends EntityPlayerSP {
       if(MCP_PlaneChaseCamera.shouldBlockNonChaseDummyWrite("MCH_ViewEntityDummy.setCameraPosition")) {
          return;
       }
+
       if(instance != null) {
-         MCP_PlaneChaseCamera.recordDummyTransformWrite("MCH_ViewEntityDummy.setCameraPosition", false);
          instance.prevPosX = x;
          instance.prevPosY = y;
          instance.prevPosZ = z;
@@ -86,103 +94,64 @@ public class MCH_ViewEntityDummy extends EntityPlayerSP {
          instance.posX = x;
          instance.posY = y;
          instance.posZ = z;
+
          instance.configureNoCollisionChaseDummy();
-         MCP_PlaneChaseCamera.logCameraWrite("MCH_ViewEntityDummy.setCameraPosition", String.format("pos=(%.3f,%.3f,%.3f)", Double.valueOf(x), Double.valueOf(y), Double.valueOf(z)));
+
+         MCP_PlaneChaseCamera.recordDummyTransformWrite("MCH_ViewEntityDummy.setCameraPosition", false);
+         MCP_PlaneChaseCamera.logCameraWrite(
+                 "MCH_ViewEntityDummy.setCameraPosition",
+                 String.format("pos=(%.3f,%.3f,%.3f)", Double.valueOf(x), Double.valueOf(y), Double.valueOf(z))
+         );
       }
    }
 
+   @Override
    public void setPosition(double x, double y, double z) {
-      if(MCP_PlaneChaseCamera.shouldBlockNonChaseDummyWrite("MCH_ViewEntityDummy.setPosition")) {
-         return;
-      }
       super.setPosition(x, y, z);
       this.configureNoCollisionChaseDummy();
-      MCP_PlaneChaseCamera.logCameraWrite("MCH_ViewEntityDummy.setPosition", String.format("pos=(%.3f,%.3f,%.3f)", Double.valueOf(x), Double.valueOf(y), Double.valueOf(z)));
    }
 
    public void configureNoCollisionChaseDummy() {
       super.noClip = true;
       super.ridingEntity = null;
       super.riddenByEntity = null;
+      super.ySize = 0.0F;
       this.setSize(0.01F, 0.01F);
-      super.boundingBox.setBounds(super.posX - 0.005D, super.posY - 0.005D, super.posZ - 0.005D, super.posX + 0.005D, super.posY + 0.005D, super.posZ + 0.005D);
-   }
 
-   public AxisAlignedBB getCollisionBox(Entity entity) {
-      return null;
-   }
-
-   public AxisAlignedBB getBoundingBox() {
-      return super.boundingBox;
-   }
-
-   public boolean canBePushed() {
-      return false;
-   }
-
-   public boolean canBeCollidedWith() {
-      return false;
-   }
-
-   public void setPosition(double x, double y, double z) {
-      if(MCP_PlaneChaseCamera.shouldBlockNonChaseDummyWrite("MCH_ViewEntityDummy.setPosition")) {
-         return;
+      if(super.boundingBox != null) {
+         super.boundingBox.setBounds(
+                 super.posX - 0.005D,
+                 super.posY - 0.005D,
+                 super.posZ - 0.005D,
+                 super.posX + 0.005D,
+                 super.posY + 0.005D,
+                 super.posZ + 0.005D
+         );
       }
-      super.setPosition(x, y, z);
-      this.configureNoCollisionChaseDummy();
-      MCP_PlaneChaseCamera.logCameraWrite("MCH_ViewEntityDummy.setPosition", String.format("pos=(%.3f,%.3f,%.3f)", Double.valueOf(x), Double.valueOf(y), Double.valueOf(z)));
    }
 
-   public void configureNoCollisionChaseDummy() {
-      super.noClip = true;
-      super.ridingEntity = null;
-      super.riddenByEntity = null;
-      this.setSize(0.01F, 0.01F);
-      super.boundingBox.setBounds(super.posX - 0.005D, super.posY - 0.005D, super.posZ - 0.005D, super.posX + 0.005D, super.posY + 0.005D, super.posZ + 0.005D);
-   }
-
+   @Override
    public AxisAlignedBB getCollisionBox(Entity entity) {
       return null;
    }
 
+   @Override
    public AxisAlignedBB getBoundingBox() {
-      return super.boundingBox;
-   }
-
-   public boolean canBePushed() {
-      return false;
-   }
-
-   public boolean canBeCollidedWith() {
-      return false;
-   }
-
-   public void configureNoCollisionChaseDummy() {
-      super.noClip = true;
-      super.ridingEntity = null;
-      super.riddenByEntity = null;
-      this.setSize(0.01F, 0.01F);
-      super.boundingBox.setBounds(super.posX - 0.005D, super.posY - 0.005D, super.posZ - 0.005D, super.posX + 0.005D, super.posY + 0.005D, super.posZ + 0.005D);
-   }
-
-   public AxisAlignedBB getCollisionBox(Entity entity) {
       return null;
    }
 
-   public AxisAlignedBB getBoundingBox() {
-      return super.boundingBox;
-   }
-
+   @Override
    public boolean canBePushed() {
       return false;
    }
 
+   @Override
    public boolean canBeCollidedWith() {
       return false;
    }
 
+   @Override
    public float getFOVMultiplier() {
-      return super.getFOVMultiplier() * (1.0F / this.zoom);
+      return super.getFOVMultiplier() * (1.0F / Math.max(this.zoom, 0.01F));
    }
-
 }
