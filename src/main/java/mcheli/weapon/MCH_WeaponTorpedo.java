469d038c5a53fc7eef96e3cebe5cacd4cb3404f0
RagexPrince683 <49804375+RagexPrince683@users.noreply.github.com>
2026-07-03 00:39:44 -0400

    Use TV guidance for guided torpedoes

--- src/main/java/mcheli/weapon/MCH_WeaponTorpedo.java
+++ src/main/java/mcheli/weapon/MCH_WeaponTorpedo.java
@@ -1,17 +1,16 @@
 package mcheli.weapon;
 
-import mcheli.MCH_Lib;
-import mcheli.weapon.MCH_EntityTorpedo;
-import mcheli.weapon.MCH_WeaponBase;
-import mcheli.weapon.MCH_WeaponInfo;
-import mcheli.weapon.MCH_WeaponParam;
-import mcheli.wrapper.W_WorldFunc;
+import mcheli.aircraft.MCH_EntityBaseVehicle;
+import mcheli.aircraft.MCH_PacketNotifyTVMissileEntity;
+import mcheli.wrapper.W_Entity;
 import net.minecraft.util.MathHelper;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 public class MCH_WeaponTorpedo extends MCH_WeaponBase {
+
+   protected MCH_EntityTorpedo lastShotGuidedTorpedo = null;
+   protected net.minecraft.entity.Entity lastShotEntity = null;
 
    public MCH_WeaponTorpedo(World w, Vec3 v, float yaw, float pitch, String nm, MCH_WeaponInfo wi) {
       super(w, v, yaw, pitch, nm, wi);
@@ -23,6 +22,28 @@ public class MCH_WeaponTorpedo extends MCH_WeaponBase {
          super.interval -= 10;
       }
 
+   }
+
+
+   public void update(int countWait) {
+      super.update(countWait);
+
+      if(!super.worldObj.isRemote) {
+         if(super.tick <= 9 && this.lastShotGuidedTorpedo != null && this.lastShotEntity != null) {
+            if(super.tick % 3 == 0 && !this.lastShotGuidedTorpedo.isDead && !this.lastShotEntity.isDead) {
+               MCH_PacketNotifyTVMissileEntity.send(W_Entity.getEntityId(this.lastShotEntity), W_Entity.getEntityId(this.lastShotGuidedTorpedo));
+            }
+
+            if(super.tick == 9) {
+               this.lastShotEntity = null;
+               this.lastShotGuidedTorpedo = null;
+            }
+         }
+
+         if(super.tick <= 2 && this.lastShotEntity instanceof MCH_EntityBaseVehicle) {
+            ((MCH_EntityBaseVehicle)this.lastShotEntity).setTVMissile(this.lastShotGuidedTorpedo);
+         }
+      }
    }
 
    public boolean shot(MCH_WeaponParam prm) {
@@ -56,63 +77,31 @@ public class MCH_WeaponTorpedo extends MCH_WeaponBase {
    }
 
    protected boolean shotGuided(MCH_WeaponParam prm) {
+      if(super.worldObj.isRemote) {
+         return true;
+      }
+
       float yaw = prm.user.rotationYaw;
       float pitch = prm.user.rotationPitch;
-      Vec3 v = MCH_Lib.RotVec3(0.0D, 0.0D, 1.0D, -yaw, -pitch, -prm.rotRoll);
-      double tX = v.xCoord;
-      double tZ = v.zCoord;
-      double tY = v.yCoord;
-      double dist = (double)MathHelper.sqrt_double(tX * tX + tY * tY + tZ * tZ);
-      if(dist < 0.001D) {
-         return false;
-      }
+      double mx = (double)(-MathHelper.sin(yaw / 180.0F * 3.1415927F) * MathHelper.cos(pitch / 180.0F * 3.1415927F));
+      double mz = (double)(MathHelper.cos(yaw / 180.0F * 3.1415927F) * MathHelper.cos(pitch / 180.0F * 3.1415927F));
+      double my = (double)(-MathHelper.sin(pitch / 180.0F * 3.1415927F));
+      mx = mx * (double)this.getInfo().acceleration + prm.entity.motionX;
+      my = my * (double)this.getInfo().acceleration + prm.entity.motionY;
+      mz = mz * (double)this.getInfo().acceleration + prm.entity.motionZ;
+      super.acceleration = MathHelper.sqrt_double(mx * mx + my * my + mz * mz);
 
-      if(super.worldObj.isRemote) {
-         tX = tX * 100.0D / dist;
-         tY = tY * 100.0D / dist;
-         tZ = tZ * 100.0D / dist;
-      } else {
-         tX = tX * 150.0D / dist;
-         tY = tY * 150.0D / dist;
-         tZ = tZ * 150.0D / dist;
-      }
-
-      Vec3 src = W_WorldFunc.getWorldVec3(super.worldObj, prm.user.posX, prm.user.posY, prm.user.posZ);
-      Vec3 dst = W_WorldFunc.getWorldVec3(super.worldObj, prm.user.posX + tX, prm.user.posY + tY, prm.user.posZ + tZ);
-      MovingObjectPosition m = W_WorldFunc.clip(super.worldObj, src, dst);
-
-      if(!super.worldObj.isRemote) {
-         double targetX = dst.xCoord;
-         double targetY = dst.yCoord;
-         double targetZ = dst.zCoord;
-
-         if(m != null && m.hitVec != null) {
-            targetX = m.hitVec.xCoord;
-            targetY = m.hitVec.yCoord;
-            targetZ = m.hitVec.zCoord;
-         }
-
-         double mx = (double)(-MathHelper.sin(yaw / 180.0F * 3.1415927F) * MathHelper.cos(pitch / 180.0F * 3.1415927F));
-         double mz = (double)(MathHelper.cos(yaw / 180.0F * 3.1415927F) * MathHelper.cos(pitch / 180.0F * 3.1415927F));
-         double my = (double)(-MathHelper.sin(pitch / 180.0F * 3.1415927F));
-         mx = mx * (double)this.getInfo().acceleration + prm.entity.motionX;
-         my = my * (double)this.getInfo().acceleration + prm.entity.motionY;
-         mz = mz * (double)this.getInfo().acceleration + prm.entity.motionZ;
-         super.acceleration = MathHelper.sqrt_double(mx * mx + my * my + mz * mz);
-         MCH_EntityTorpedo e = new MCH_EntityTorpedo(super.worldObj, prm.posX, prm.posY, prm.posZ, prm.entity.motionX, prm.entity.motionY, prm.entity.motionZ, yaw, 0.0F, (double)super.acceleration);
-         e.setName(super.name);
-         e.setParameterFromWeapon(this, prm.entity, prm.user);
-         e.targetPosX = targetX;
-         e.targetPosY = targetY;
-         e.targetPosZ = targetZ;
-         e.motionX = mx;
-         e.motionY = my;
-         e.motionZ = mz;
-         e.accelerationInWater = this.getInfo() != null?(double)this.getInfo().accelerationInWater:1.0D;
-         super.worldObj.spawnEntityInWorld(e);
-         this.playSound(prm.entity);
-      }
-
+      MCH_EntityTorpedo e = new MCH_EntityTorpedo(super.worldObj, prm.posX, prm.posY, prm.posZ, mx, my, mz, yaw, pitch, (double)super.acceleration);
+      e.setName(super.name);
+      e.setParameterFromWeapon(this, prm.entity, prm.user);
+      e.motionX = mx;
+      e.motionY = my;
+      e.motionZ = mz;
+      e.accelerationInWater = this.getInfo() != null?(double)this.getInfo().accelerationInWater:1.0D;
+      this.lastShotEntity = prm.entity;
+      this.lastShotGuidedTorpedo = e;
+      super.worldObj.spawnEntityInWorld(e);
+      this.playSound(prm.entity);
       return true;
    }
 }
